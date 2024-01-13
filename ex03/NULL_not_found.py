d3d4bd7d55ea5ec8d48b92d153713cc89a0bf67e
Woolim Park <woolim@getluko.com>
2024-01-13 11:31:50 +0100

    init

---
+++ ex03/NULL_not_found.py
@@ -0,0 +1,15 @@
+def NULL_not_found(object):
+	if object == None:
+			print(f"Nothing: {object} {type(object)}")
+	elif type(object) is float and object != object: 
+			print(f"Cheese: {object} {type(object)}")
+	elif type(object) is int and not object:
+			print(f"Zero: {object} {type(object)}")
+	elif type(object) is str and not object:
+			print(f"Empty: {type(object)}")
+	elif type(object) is bool and not object:
+			print(f"Fake: {object} {type(object)}")
+	else:
+			print("Type not Found")
+			return 1
+	return 0
